2018cb99e8b6020d179a332bb3f9d50c1d637892
mubassir.care <mubassir.care@gmail.com>
2023-03-11 11:26:39 +0600

    fix remote

---
+++ BusinessEntity/Auth/Queries/Login/UserLoginQuery.cs
@@ -0,0 +1,25 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessEntity.Auth.Queries.Login
+{
+    public class UserLoginQuery:IRequest<List<Claim>>
+    {
+        public string? UserName { get; set; }
+        public string? Password { get; set;}
+        public bool RememberMe { get; set; }
+    }
+
+    public class UserLoginQueryHandler : IRequestHandler<UserLoginQuery, List<Claim>>
+    {
+        public async Task<List<Claim>> Handle(UserLoginQuery request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Domain/DatabaseConnectionContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Domain
+{
+    public class DatabaseConnectionContext
+    {
+        private readonly IConfiguration _configuration;
+
+        public DatabaseConnectionContext(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        public IDbConnection CreateMartConnection() => new SqlConnection(_configuration.GetConnectionString("DBConStrLinkage"));
+    }
+}
